6ef753b17e6bd85b27065d7089f896712c30d47c
sapphire <info@sapphire.sh>
2019-05-31 02:19:20 +0900

    init xcode project

---
+++ onNetworkChange/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  onNetworkChange
+//
+//  Created by sapphire on 31/05/2019.
+//  Copyright © 2019 sapphire. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
